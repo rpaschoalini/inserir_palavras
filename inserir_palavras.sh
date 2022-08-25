b317dca4bd00f460184e6dee7fefd41367bba39a
root <root@ip-10-0-2-251.ec2.internal>
2022-08-25 12:06:49 +0000

    Primeira versao do script

---
+++ inserir_palavras.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#echo "VPN XXXXX" >> /caminho/arquivo.extensao
+#echo "INSERIR O COMANDO PUSH ENTRE ASPAS" >> /caminho/arquivo.extensao
+
+validacao=Arquivo_Modificado
+
+echo "VPN XXXXX" >> /caminho/arquivo.extensao
+echo "INSERIR O COMANDO PUSH ENTRE ASPAS" >> /caminho/arquivo.extensao
+echo $validacao
+
+echo "VPN XXXXX" >> /caminho/arquivo.extensao
+echo "INSERIR O COMANDO PUSH ENTRE ASPAS" >> /caminho/arquivo.extensao
+echo $validacao
+
+echo "VPN XXXXX" >> /caminho/arquivo.extensao
+echo "INSERIR O COMANDO PUSH ENTRE ASPAS" >> /caminho/arquivo.extensao
+echo $validacao
